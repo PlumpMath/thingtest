e45dc1f1dcbc5e25354b0281c5070d2ff97186a6
Adam Moore <adm@semitransparentdesign.com>
2016-09-27 13:17:43 +0900

    Add second lesson, change view switching

--- src/thingtest/core.cljs
+++ src/thingtest/core.cljs
@@ -11,19 +11,20 @@
    [thi.ng.geom.gl.buffers :as buf]
    [thi.ng.geom.gl.webgl.animator :as anim]
    [thi.ng.geom.gl.shaders.phong :as phong]
-   [thi.ng.geom.gl.shaders :as shd]
-
-   ))
+   [thi.ng.geom.gl.shaders :as shd]))
 
 (enable-console-print!)
 
-(defonce app (reagent/atom {:selected 0}))
+(defonce app (reagent/atom {:selected 1}))
+
+;; Lesson 1
+;;---------------------------------------------------------------------------
 
 (def shader-0-spec
   {:vs
    "
     void main(void) {
-        gl_Position = vec4(aVertexPosition, 1.0);
+        gl_Position = vec4(position, 1.0);
     }
     "
    :fs
@@ -33,26 +34,18 @@
     }
    "
 
-   :uniforms {
-              :color [:vec3 [1 1 0]]
-              }
-   :attribs {
-             :aVertexPosition :vec3
-             }
-   })
+   :uniforms {:color [:vec3 [1 1 0]]}
+   :attribs {:position :vec3}})
 
 (defn init-0
   [this]
   (let [ctx (gl/gl-context (reagent/dom-node this))]
     (let [shader (shd/make-shader-from-spec ctx shader-0-spec)
           spec
-          {:attribs {:position {
-                                :data (js/Float32Array. [0,1,0,
-                                                         -1,-1,0,
+          {:attribs {:position {:data (js/Float32Array. [0,1,0
+                                                         -1,-1,0
                                                          1,-1,0])
-                                :size 3
-                                }
-                     },
+                                :size 3}}
            :mode glc/triangles
            :num-vertices 3}
           ]
@@ -60,22 +53,81 @@
              :ctx ctx
              :spec
              (assoc (gl/make-buffers-in-spec spec ctx glc/static-draw)
-                    :shader shader)
-             )
-             )))
+                    :shader shader)))))
 
 (defn update-0
   [this]
   (fn [t frame]
-    (let [{:keys [ctx shader spec]} @app
-          view-rect (gl/get-viewport-rect ctx)]
+
+    (let [{:keys [ctx shader spec]} @app]
+
       (when ctx
-        (gl/set-viewport ctx view-rect)
         (gl/clear-color-buffer ctx 0 1 0 1)
-        (gl/draw-with-shader ctx spec
-                             )))
+        (gl/draw-with-shader ctx spec)))
     (:active (reagent/state this))))
 
+
+;; Lesson 2
+;;---------------------------------------------------------------------------
+
+
+(def shader-1-spec
+  {:vs
+   "
+    void main(void) {
+        gl_Position = vec4(position, 1.0);
+        vColor = vec4(color, 1.0);
+    }
+    "
+   :fs
+   "
+    void main(void) {
+        gl_FragColor = vColor;
+    }
+   "
+
+   :varying {
+             :vColor :vec4
+             }
+   :attribs {
+             :position :vec3
+             :color :vec3
+             }})
+
+(defn init-1
+  [this]
+  (let [ctx (gl/gl-context (reagent/dom-node this))]
+    (let [shader (shd/make-shader-from-spec ctx shader-1-spec)
+          spec
+          {:attribs {:position {:data (js/Float32Array. [0, 1,0
+                                                        -1,-1,0
+                                                         1,-1,0])
+                                :size 3}
+                     :color {:data (js/Float32Array.
+                                    [1, 0, 0
+                                     0, 1, 0
+                                     0, 0, 1
+                                     ])
+                             :size 3}}
+           :mode glc/triangles
+           :num-vertices 3}]
+      (swap! app assoc
+             :ctx ctx
+             :spec
+             (assoc (gl/make-buffers-in-spec spec ctx glc/static-draw)
+                    :shader shader)))))
+
+(defn update-1
+  [this]
+  (fn [t frame]
+    (let [{:keys [ctx shader spec]} @app]
+      (when ctx
+        (gl/clear-color-buffer ctx 0 0 0 1)
+        (gl/draw-with-shader ctx spec)))
+    (:active (reagent/state this))))
+
+;; Common components
+;;---------------------------------------------------------------------------
 
 (defn gl-component
   [props]
@@ -89,33 +141,39 @@
     (fn [this]
       (reagent/set-state this {:active false}))
     :reagent-render
-    (fn [_]
+    (fn [props]
       [:canvas
        (merge
         {:width (:width props)
-         :height (:height props)
-         }
+         :height (:height props)}
         props)])}))
 
+(defn lesson0 []
+  [gl-component {:init init-0 :loop update-0 :width 640 :height 640 :selected (:selected @app)}]
+  )
+
+(defn lesson1 []
+  [gl-component {:init init-1 :loop update-1 :width 640 :height 640 :selected (:selected @app)}]
+  )
 
 (defn app-component
   []
-  (println @app)
   [:div
    [:h2 "controls"]
    (into [:ul.menu]
-         (for [n (range 1)]
+         (for [n (range 2)]
            [:li {:class (when (= n (:selected @app)) "active")}
             [:a {:href "#"
                  :on-click (fn [e]
                              (.preventDefault e)
-                             (reset! app {:selected n}))
-                 }
-             (str "Lesson " n)]]
-           ))
-   (case (:selected @app)
-     0 [gl-component {:init init-0 :loop update-0 :width 640 :height 640}]
-     [:h3 "Unknown."])])
+                             (reset! app {:selected n}))}
+             (str "Lesson " n)]]))
+   [:h4 "Selected: " (:selected @app)]
+   [:div (case (:selected @app)
+           0 [lesson0]
+           1 [lesson1]
+           :else
+           [:h3 "Unknown."])]])
 
 (defn main
   []
